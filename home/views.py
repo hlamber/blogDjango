649c1783e48874616780cf4a9dc80daad1c7183c
Laura <laura@MacBook-Pro-de-Laura-3.local>
2021-03-09 16:22:14 +0100

    Correction erreur conflits

--- home/views.py
+++ home/views.py
@@ -1,6 +1,5 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import TemplateView
 
 # Create your views here.
 # def home(request):
@@ -9,13 +8,5 @@ from django.views.generic import TemplateView
 #     <p>Ceci est ma première page avec django !</p>
 #     """)
 
-<<<<<<< HEAD:home/views.py
-# class HomePageView(TemplateView):
-#     template_name = "home/index.html"
-
 def home(request):
     return render(request, 'home/index.html', {})
-=======
-class HomePageView(TemplateView):
-    template_name = "home/index.html"
->>>>>>> 2394ac2b8f032f81478daff64a8c1a9f73eeffb8:home/views.py
--- mySite/urls.py
+++ mySite/urls.py
@@ -6,11 +6,7 @@ urlpatterns = [
     
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-<<<<<<< HEAD
     path('', include('home.urls')),
-=======
-    url(r'^', include('home.urls')),
->>>>>>> 2394ac2b8f032f81478daff64a8c1a9f73eeffb8
     path('', include('django.contrib.auth.urls')),
     url(r'^', include('about.urls')), # tell django to read urls.py in example app
 
