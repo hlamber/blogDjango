21d527be6514eebd8e08906523f268e97fcf8617
Léna ENG <55486985+sienning@users.noreply.github.com>
2021-03-10 16:37:29 +0100

    sqlite3 -> postgresql

--- mySite/settings.py
+++ mySite/settings.py
@@ -81,13 +81,24 @@ WSGI_APPLICATION = 'mySite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
+DATABASES = {
+  'default': {
+      'ENGINE': 'django.db.backends.postgresql_psycopg2',
+      'NAME': 'cv',
+      'USER': 'admin',
+      'PASSWORD': 'mdp',
+      'HOST': 'localhost',
+    #   'HOST': '127.0.0.1',
+      'PORT': '',
+  }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
